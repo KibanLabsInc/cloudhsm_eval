2a36516eddba68f01fbea66ac0be5a558c5d42dd
Nickolas Heckman <nickolas.heckman@gmail.com>
2018-08-29 11:46:35 -0700

    Instantiate, run and collect timing for simulator runnable

--- src/main/java/io/afero/cloudhsm/v2/Main.java
+++ src/main/java/io/afero/cloudhsm/v2/Main.java
@@ -1,9 +1,11 @@
 package io.afero.cloudhsm.v2;
 
+import io.afero.cloudhsm.v2.simulator.LoadSimulator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author nrheckman 8/29/18 10:34 AM
@@ -14,5 +16,24 @@ public class Main {
 	public static void main(String[] args) {
 		LOG.info("Application started with arguments, " + Arrays.toString(args));
 
+		if (args.length < 3) {
+			LOG.error("Expected application arguments <keyAlias>, <rate>, <count> were not found. " +
+					"If running from gradle, use --args '<keyAlias> <rate> <count>'");
+			System.exit(1);
+		}
+
+		String keyAlias = args[0];
+		int rate = Integer.parseInt(args[1]);
+		int count = Integer.parseInt(args[2]);
+
+		Runnable simulator = new LoadSimulator(keyAlias, rate, count);
+
+		long simulatorStartNanos = System.nanoTime();
+		simulator.run();
+
+		LOG.debug("Simulator total execution ms: " +
+				TimeUnit.MILLISECONDS.convert(
+						System.nanoTime() - simulatorStartNanos,
+						TimeUnit.NANOSECONDS));
 	}
 }
---
+++ src/main/java/io/afero/cloudhsm/v2/simulator/LoadSimulator.java
@@ -0,0 +1,26 @@
+package io.afero.cloudhsm.v2.simulator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author nrheckman 8/29/18 11:13 AM
+ */
+public class LoadSimulator implements Runnable {
+	private static final Logger LOG = LogManager.getLogger(LoadSimulator.class);
+
+	private final String keyAlias;
+	private final int rate;
+	private final int count;
+
+	public LoadSimulator(String keyAlias, int rate, int count) {
+		this.keyAlias = keyAlias;
+		this.rate = rate;
+		this.count = count;
+	}
+
+	@Override
+	public void run() {
+
+	}
+}
