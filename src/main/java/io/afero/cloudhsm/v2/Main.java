ef20f82f6ecb615c4ac0ffc8d57b27c1aa05053c
Nickolas Heckman <nickolas.heckman@gmail.com>
2018-08-29 13:27:19 -0700

    Simulates signing requests with given concurrency for given count

--- src/main/java/io/afero/cloudhsm/v2/Main.java
+++ src/main/java/io/afero/cloudhsm/v2/Main.java
@@ -1,5 +1,6 @@
 package io.afero.cloudhsm.v2;
 
+import io.afero.cloudhsm.v2.keystore.KeyStoreFactory;
 import io.afero.cloudhsm.v2.simulator.LoadSimulator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,23 +19,28 @@ public class Main {
 		LOG.info("Application started with arguments, " + Arrays.toString(args));
 
 		if (args.length < 3) {
-			LOG.error("Expected application arguments <user> <keyAlias>, <concurrency>, <count> were not found. " +
-					"If running from gradle, use --args '<user> <keyAlias> <concurrency> <count>'");
+			LOG.error("Expected application arguments <provider> <user> <keyAlias>, <concurrency>, <count> were not found. " +
+					"If running from gradle, use --args '<provider> <user> <keyAlias> <concurrency> <count>'");
 			System.exit(1);
 		}
 
-		String user = args[0];
-		String keyAlias = args[1];
-		int concurrency = Integer.parseInt(args[2]);
-		int count = Integer.parseInt(args[3]);
+		String provider = args[0];
+		String user = args[1];
+		String keyAlias = args[2];
+		int concurrency = Integer.parseInt(args[3]);
+		int count = Integer.parseInt(args[4]);
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Key Alias Password? ");
 		String password = scanner.nextLine();
 
+		KeyStoreFactory keyStoreFactory = new KeyStoreFactory();
+
 		Runnable simulator = null;
 		try {
-			simulator = new LoadSimulator(user, password, keyAlias, concurrency, count);
+			simulator = new LoadSimulator(
+					keyStoreFactory.create(provider, user, password),
+					user, password, keyAlias, concurrency, count);
 		} catch (Exception e) {
 			LOG.fatal("Unable to instantiate simulator", e);
 			System.exit(1);
--- src/main/java/io/afero/cloudhsm/v2/simulator/LoadSimulator.java
+++ src/main/java/io/afero/cloudhsm/v2/simulator/LoadSimulator.java
@@ -3,8 +3,11 @@ package io.afero.cloudhsm.v2.simulator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.security.KeyStore;
-import java.security.Provider;
+import java.security.*;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import static java.lang.Class.forName;
 
@@ -14,38 +17,67 @@ import static java.lang.Class.forName;
 public class LoadSimulator implements Runnable {
 	private static final Logger LOG = LogManager.getLogger(LoadSimulator.class);
 
-	private final String keyAlias;
-	private final int concurrency;
 	private final int count;
-	private final KeyStore keyStore;
+	private final ExecutorService executorService;
+	private final SignatureTimingSupplierFactory signatureTimingSupplierFactory;
 
-	public LoadSimulator(String user, String password, String keyAlias, int concurrency, int count) throws Exception {
-		this.keyAlias = keyAlias;
-		this.concurrency = concurrency;
+	public LoadSimulator(KeyStore keyStore, String user, String password, String keyAlias, int concurrency, int count) throws Exception {
 		this.count = count;
 
-		keyStore = createKeyStore(user, password);
+		executorService = Executors.newFixedThreadPool(concurrency);
+		signatureTimingSupplierFactory = new SignatureTimingSupplierFactory(keyStore, keyAlias, user + ":" + password);
 	}
 
 	@Override
 	public void run() {
+		Consumer<Long> timingConsumer = (nanos) -> {
+			LOG.trace("Sign took " + nanos);
+		};
 
+		for (int i = 0; i < count; i++) {
+			CompletableFuture.supplyAsync(signatureTimingSupplierFactory.create(), executorService)
+					.thenAccept(timingConsumer);
+		}
 	}
 
-	private KeyStore createKeyStore(String user, String password) throws Exception {
-		LOG.trace("Creating KeyStore backed by Cavium provider");
+	private static class SignatureTimingSupplierFactory {
+		private static final Random random = new SecureRandom();
+		private final KeyStore keyStore;
+		private final String keyAlias;
+		private final String credentials;
 
-		System.setProperty("HSM_PARTITION", "PARTITION_1");
-		System.setProperty("HSM_USER", user);
-		System.setProperty("HSM_PASSWORD", password);
+		private SignatureTimingSupplierFactory(KeyStore keyStore, String keyAlias, String credentials) {
+			this.keyStore = keyStore;
+			this.keyAlias = keyAlias;
+			this.credentials = credentials;
+		}
 
-		Class<?> caviumProviderClass = forName("com.cavium.provider.CaviumProvider");
-		Provider provider = (Provider)caviumProviderClass.newInstance();
-		java.security.Security.addProvider(provider);
+		Supplier<Long> create() {
+			return new SignatureTimingSupplier();
+		}
 
-		KeyStore keyStore = KeyStore.getInstance("cavium", provider);
-		keyStore.load(null, (user + ":" + password).toCharArray());
+		private class SignatureTimingSupplier implements Supplier<Long> {
 
-		return keyStore;
+			@Override
+			public Long get() {
+				byte[] bytes = new byte[32];
+				random.nextBytes(bytes);
+
+				long nanos;
+				try {
+					Signature signatureInstance = Signature.getInstance("NONEwithECDSA");
+					signatureInstance.initSign((PrivateKey)keyStore.getKey(keyAlias, credentials.toCharArray()));
+
+					nanos = System.nanoTime();
+					signatureInstance.update(bytes);
+					signatureInstance.sign();
+				} catch (Exception e) {
+					throw new CompletionException(e);
+				}
+
+				return System.nanoTime() - nanos;
+			}
+		}
+
 	}
 }
